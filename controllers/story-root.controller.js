9b1eb64c896588461eb728a10daaa4f478a46485
gergelykovacs89 <gergelyk89@gmail.com>
2019-03-24 15:32:50 +0100

    add fetchStoryRoot with author and rootText in response

--- controllers/story-root.controller.js
+++ controllers/story-root.controller.js
@@ -1,6 +1,7 @@
 const _ = require("lodash");
 const { Text } = require("../models/text");
 const { StoryRoot } = require("../models/story-root");
+const { Author } = require("../models/author");
 
 exports.createRoot = async function(req, res) {
   try {
@@ -39,7 +40,9 @@ exports.getRootById = async function(req, res) {
   try {
     let storyRootId = req.params.id;
     const root = await StoryRoot.findById(storyRootId);
-    res.status(200).send(root);
+    const author = await Author.findById(root._authorId);
+    const rootText = await Text.findById(root._rootTextId);
+    res.status(200).send({ root, author, rootText });
   } catch (e) {
     res.status(400).send({
       status: "Somethign went wrong..."
